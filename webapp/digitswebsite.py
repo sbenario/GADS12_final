518fa3a3cb25d2f9b98b779f211f745f64892ff4
Steven Benario <sbenario@gmail.com>
2014-10-21 14:59:28 -0400

    we now have the same base64 encoded image on both client and server. success.

--- webapp/digitswebsite.py
+++ webapp/digitswebsite.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from sklearn.externals import joblib
 import digitClassifier
 import base64
+import StringIO
 
 app = Flask(__name__)
 
@@ -29,31 +30,53 @@ def index():
 @app.route('/input', methods=['POST'])
 def numberInput():
 	inputdata =  request.form  #type is unfortunately werkzeug.datastructures.CombinedMultiDict
-			#GAH!  Looks like now we could use request.get_data, which would work too.
+	
+	print 'type of inputdata ', type(inputdata)
+	print inputdata
+
 	#the following will pull our one key out of this weird data structure
 	rawData = ""
+	print 'input keys: ', len(inputdata.keys())
 	for key in inputdata.keys():
-		rawData = key
+		rawData = inputdata[key]
 
-	#rawData is the raw data over the wire, which is double-encoded.
-	print rawData
-	# print type(rawData)
-	print
-
-	#we have discovered that often times the data comes back with bad amounts of padding	
-	decodedOnce = digitClassifier.decodeBadPaddingBase64(rawData)
-
-	#now that we've decoded it once, it should work as expected.
-
-	# digitClassifier.writeImageToFile(imageBase64, 'testoutput.png')
+	# stringObject = StringIO.StringIO(rawData)
 	print
 	print
 	print
-
+	print 'my rawdata:', rawData  #rawdata should now be a binary blob
+	print
+	print
+	print len(rawData)
+	print type(rawData)    #RAWDATA IS NOW THE SAME BASE64 STRING AS ON THE CLIENT!!!
+	t = open('binarypng.png', 'wb')
+	t.write(rawData)
+	t.close()
+	print
+	print base64.encodestring(rawData)
+	print
 	imageArray = digitClassifier.convertBase64ToImageArray(rawData)
-	scaledImageArray = digitClassifier.scaleImageTo20px(imageArray)
 
-	prediction = digitClassifier.predictDigit(knn, scaledImageArray)
+	# #rawData is the raw data over the wire, which is double-encoded.
+	# # print str(rawData)
+	# # print type(rawData)
+	# print
+
+	# #we have discovered that often times the data comes back with bad amounts of padding	
+	# # decodedOnce = digitClassifier.decodeBadPaddingBase64(rawData)
+
+	# #now that we've decoded it once, it should work as expected.
+
+	# # digitClassifier.writeImageToFile(imageBase64, 'testoutput.png')
+	# print
+	# print
+	# print
+
+	# imageArray = digitClassifier.convertBase64ToImageArray(rawData)
+	# scaledImageArray = digitClassifier.scaleImageTo20px(imageArray)
+
+	# prediction = digitClassifier.predictDigit(knn, scaledImageArray)
+	prediction = "Pass through successful"
 
 	return render_template('results.html', result=prediction )
 
